ee8c8159588f5cefde58db97371661db6fe008c3
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-03-13 18:51:58 +0530

    Add pie chart of programming languages

---
+++ programmingLangFront.js
@@ -0,0 +1,21 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Language', 'Experience'],
+        ['JavaScript',     11],
+        ['Java',      11],
+        ['C/C++',  6],
+        ['JSON',1]
+    ]);
+
+    var options = {
+        title: 'My Programming Languages'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+}
