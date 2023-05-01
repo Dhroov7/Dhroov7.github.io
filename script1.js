01460035363d7defdfc398a1b2de5b2842b8d225
Dhroov Gupta <dhroovgupta@Dhroovs-MacBook-Pro.local>
2023-05-01 20:21:40 +0530

    Revamp Portfolio

--- script1.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var owl = $("#owl-demo");
-
-    owl.owlCarousel({
-        navigation : true,
-        singleItem : true,
-        transitionStyle : "fade"
-    });
-})
